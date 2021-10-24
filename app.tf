0d527518c760f7f1226ef902cffab2554fda4f77
jesusvillalobos1 <j.villalobos.fly.com>
2021-10-24 14:33:30 -0500

    updating app.tf

--- app.tf
+++ app.tf
@@ -72,3 +72,38 @@ resource "aws_subnet" "database-subnet-2" {
     Name = "Database-2b"
   }
 }
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.app_vpc.id
+ 
+  tags = {
+    Name = "Demo IGW"
+  }
+}
+ 
+# Create Web layer route table
+resource "aws_route_table" "web-rt" {
+  vpc_id = aws_vpc.app_vpc.id
+ 
+ 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+ 
+  tags = {
+    Name = "WebRT"
+  }
+}
+ 
+# Create Web Subnet association with Web route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.web-subnet-1.id
+  route_table_id = aws_route_table.web-rt.id
+}
+ 
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.web-subnet-2.id
+  route_table_id = aws_route_table.web-rt.id
+}
