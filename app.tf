bcfc426b59899ae6dc9eb3e2ede5641d53172ad8
Rafael Rojas <root@rafaelrojas.net>
2021-10-26 09:43:23 -0500

    added: ASG, launch template, load balancer, web SG

--- app.tf
+++ app.tf
@@ -108,29 +108,115 @@ resource "aws_route_table_association" "web_net_assoc2" {
   route_table_id = aws_route_table.web-rt.id
 }
 
+# Web - Application Load Balancer
+resource "aws_lb" "app_lb" {
+  name = "app-lb"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.alb_http.id]
+  subnets = [aws_subnet.web-subnet-1.id, aws_subnet.web-subnet-1.id]
+}
+
+# Web - ALB Security Group
+resource "aws_security_group" "alb_http" {
+  name        = "alb-security-group"
+  description = "Allowing HTTP requests to the application load balancer"
+  vpc_id = aws_vpc.app_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-security-group"
+  }
+}
+
+# Web - Listener
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_target_group.arn
+  }
+}
+
+# Web - Target Group
+resource "aws_lb_target_group" "web_target_group" {
+  name     = "web-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.app_vpc.id
+
+  health_check {
+    port     = 80
+    protocol = "HTTP"
+  }
+}
+
+# Web - EC2 Instance Security Group
+resource "aws_security_group" "web_instance_sg" {
+  name        = "web-server-security-group"
+  description = "Allowing requests to the web servers"
+  vpc_id = aws_vpc.app_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb_http.id]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-server-security-group"
+  }
+}
+
+# Web - Launch Template
+resource "aws_launch_template" "web_launch_template" {
+  name_prefix   = "web-launch-template"
+  #Image id should not be hardcoded
+  image_id      = "ami-074cce78125f09d61"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.rafael_key.id
+}
+
 ###This key must be encrypted
 resource "aws_key_pair" "rafael_key" {
   key_name   = "rafael_key"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEb5x7YXs73HpA5Keyo1E/qRbzL98jR8knONXmh8yEdv Rafael Rojas big-bertha"
 }
 
-#Hardcoded AMI image, it shoud have a dictionary for dynamic find
-resource "aws_launch_configuration" "app_lcfg" {
-  name_prefix = "app_lcfg"
+# Web - Auto Scaling Group
+resource "aws_autoscaling_group" "web_asg" {
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+  target_group_arns = [aws_lb_target_group.web_target_group.arn]
+  vpc_zone_identifier = [aws_subnet.web-subnet-1.id, aws_subnet.web-subnet-1.id]
 
-  image_id                    = "${data.aws_ami.appserver.id}"
-  instance_type               = var.appserver_instance_type
-  key_name                    = aws_key_pair.app_key.key_name
-  security_groups             = [aws_security_group.application_sg.id]
-  #associate_public_ip_address = true
-
-  root_block_device {
-    volume_size           = 25
-    volume_type           = "standard"
-    delete_on_termination = true
-  }
-
-  lifecycle {
-    create_before_destroy = true
+  launch_template {
+    id      = aws_launch_template.web_launch_template.id
+    version = "$Latest"
   }
 }
