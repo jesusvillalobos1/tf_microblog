59cd3a830a69b2d5bbf8c19786194913b740a23f
jesusvillalobos1 <j.villalobos.fly.com>
2021-10-31 12:21:31 -0600

    adding web listener and target group

--- app.tf
+++ app.tf
@@ -154,3 +154,29 @@ resource "aws_security_group" "alb_http" {
     Name = "alb-security-group"
   }
 }
+
+
+# Web - Listener
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_target_group.arn
+  }
+}
+
+# Web - Target Group
+resource "aws_lb_target_group" "web_target_group" {
+  name     = "web-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.app_vpc.id
+
+  health_check {
+    port     = 80
+    protocol = "HTTP"
+  }
+}
