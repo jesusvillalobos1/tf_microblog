deb793dd9a4983adb4b0f8617fbbb28693962ff7
Rafael Rojas <rafaelrojas7752@gmail.com>
2021-12-03 10:14:36 -0600

    testing provider.tf env variables

--- provider.tf
+++ provider.tf
@@ -1,18 +1,8 @@
 ####This must change to a more secure env.
+# Using environment variables for terraform cloud
+#to tets in console please port variables:
+# export AWS_SECRET_ACCESS_KEY=XXXXXXXXXXXXXXXXXXXXXXXX
+# export AWS_ACCESS_KEY_ID=XXXXXXXXXXXXXXXX
+# export AWS_DEFAULT_REGION=XXXXX
 
-variable "TF_VAR_AWS_ACCESS_KEY_ID" {}
-variable "TF_VAR_AWS_SECRET_ACCESS_KEY" {}
-variable "TF_VAR_REGION" {}
-
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.region
-}
-
-
-
-#provider "aws" {
-#  profile    =  "terraform"
-#  region     = "us-east-2"
-#}
+provider "aws" {}
