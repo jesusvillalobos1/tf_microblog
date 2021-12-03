5d9c9d21815e1118fbb618709deb21b927daa508
Rafael Rojas <rafaelrojas7752@gmail.com>
2021-12-03 09:39:13 -0600

    testing provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,18 @@
 ####This must change to a more secure env.
+
+variable "TF_VAR_AWS_ACCESS_KEY_ID" {}
+variable "TF_VAR_AWS_SECRET_ACCESS_KEY" {}
+variable "TF_VAR_REGION" {}
+
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = var.region
 }
+
+
+
 #provider "aws" {
 #  profile    =  "terraform"
 #  region     = "us-east-2"
-}
+#}
