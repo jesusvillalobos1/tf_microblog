6190926554f07635e75fa8d90eadebbeeb580903
Rafael Rojas <rafaelrojas7752@gmail.com>
2021-12-03 09:27:16 -0600

    modifying provider.rf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,10 @@
 ####This must change to a more secure env.
 provider "aws" {
-  profile    =  "terraform"
-  region     = "us-east-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
+}
+#provider "aws" {
+#  profile    =  "terraform"
+#  region     = "us-east-2"
 }
