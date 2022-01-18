ac27d9241fbd644dc341181c84372dc5b31010bc
Rafael Rojas <rafaelrojas7752@gmail.com>
2022-01-18 16:34:20 -0600

    adding backend config

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
 # Example TF configuration for dev environment
 # It will deploy all infra on a dev stage
 
+# Use terraform cloud as a backend  UNCOMMENT UNTIL PROOF OF CONCEPT IS DONE
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "big-app"
+
+    workspaces {
+      name = "big-project"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
 }
